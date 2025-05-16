f1e5658e8c921671acc9b50e4badf6da2221385d
leiruz95 <elzuri095@gmail.com>
2025-05-16 11:24:19 -0600

    MODIFICACÓN EN EL APARTADOCLENTES, QUITANDO LOS PUNTOS DE COLORES Y ACTUALIZA LA TABLA CUANDO SELECCIONA UN CLIENTE, IGUAL CON EL APARTADO NOTAS.

--- optifiber/src/components/clients/notes/Client.infoNotes.jsx
+++ optifiber/src/components/clients/notes/Client.infoNotes.jsx
@@ -3,17 +3,23 @@ function ClientNoteInfo({ note }) {
         <div className="modal fade" id="NoteModal" tabIndex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
             <div className="modal-dialog">
                 <div className="modal-content">
+
                     <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="ModalLabel">Modal title</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h1 className="modal-title fs-5" id="ModalLabel">Detalle de Nota</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
                     </div>
+
                     <div className="modal-body">
-                        ...
+                        <p><strong>Nota:</strong></p>
+                        <p>{note?.Description || 'Sin contenido disponible.'}</p>
+                        <hr />
+                        <p><strong>Fecha:</strong> {note?.CreateDate?.split("T")[0] || 'Sin fecha registrada.'}</p>
                     </div>
+
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                     </div>
+
                 </div>
             </div>
         </div>
