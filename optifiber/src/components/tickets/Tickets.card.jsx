f4bf3fcc92ce6325e891ba0debf9a13e4aa73a8a
lili <Lili678>
2025-05-21 23:19:38 -0600

    Realizacion de filtro al dar clic en folio,cliente,estado y fecha asc-desc

--- optifiber/src/components/tickets/Tickets.card.jsx
+++ optifiber/src/components/tickets/Tickets.card.jsx
@@ -5,11 +5,13 @@ import { useState } from 'react';
 
 function TicketsCard({ tickets = [], onSelected }) {
     const [search, setSearch] = useState('')
+    const [sortField, setSortField]=useState('')
+    const [sortOrder,setSortOrder]=useState('')
 
-    const handleInputSearch = (e) => {
+    const handleInputSearch = (e) => {  
         setSearch(e.target.value);
-    }
-//Filtro por estado, folio y fecha
+    };
+    //Filtro por estado, folio y fecha
     const filteredName = tickets.filter(ticket => {
     const folio = ticket.Folio?.toString().toLowerCase() || '';
     const estado = ticket.Status?.toLowerCase() || '';
@@ -31,6 +33,47 @@ function TicketsCard({ tickets = [], onSelected }) {
         fecha.includes(searchLower)
     );
 });
+    //Realiza el ordenamiento
+    const handleSort=(field)=>{
+            if (sortField===field){
+                setSortOrder(sortOrder==='asc' ? 'desc' : 'asc');
+            }
+            else {
+                setSortField(field);
+                setSortOrder('asc');
+            }
+    };
+
+      const sortedTickets = [...filteredName].sort((a, b) => {
+        const getClientName = (client) => (
+            `${client.Name.FirstName} ${client.Name.SecondName || ''} ${client.LastName.FatherLastName} ${client.LastName.MotherLastName}`
+                .replace(/\s+/g, ' ').trim().toLowerCase()
+        );
+        let valueA,ValueB;
+        switch (sortField){
+            case 'cliente':
+                valueA=getClientName(a.Client);
+                ValueB=getClientName(b.Client);
+                break;
+            case 'folio':
+                valueA=a.Folio?.toString().toLowerCase();
+                ValueB=b.Folio?.toString().toLowerCase();
+                break;
+            case 'estado':
+                valueA=a.Status?.toLowerCase();
+                ValueB=b.Status?.toLowerCase();
+                break;
+            case 'fecha':
+                valueA=a.CreateDate?.split("T")[0];
+                ValueB=b.CreateDate?.split("T")[0];
+                break;
+            default:
+                return 0;
+        }
+    return sortOrder === 'asc'
+        ? valueA.localeCompare(ValueB, 'es', { sensitivity: 'base' })
+        : ValueB.localeCompare(valueA, 'es', { sensitivity: 'base' });
+    });
 
     return (
         <div className="d-flex justify-content-center align-content-center row">
@@ -50,20 +93,25 @@ function TicketsCard({ tickets = [], onSelected }) {
                 </div>
             </div>
             
-            
+            {/* modificaciones click */}
                 <table className="table table-hover justify-content-center">
                     <thead className={styleCard['head-table']}>
                         <tr>
-                            <th>Folio</th>
-                            <th>Cliente</th>
+                            <th onClick={()=>handleSort('folio')}style={{ cursor: 'pointer' }}
+                                >Folio</th>
+                            <th onClick={()=>handleSort('cliente')}style={{ cursor: 'pointer' }}
+                                >Cliente</th>
                             <th>Prioridad</th>
                             <th>Asunto</th>
-                            <th>Estado</th>
-                            <th>Fecha</th>
+                            <th onClick={()=>handleSort('estado')}style={{ cursor: 'pointer' }}
+                                >Estado</th>
+                            <th onClick={()=>handleSort('fecha')}style={{ cursor: 'pointer' }}
+                                >Fecha</th>
                         </tr>
                     </thead>
+                    
                     <tbody className={`text-wrap ${styleCard['table-body']}`}>
-                        {filteredName.map((item) => (
+                        {sortedTickets.map((item) => (
                             <tr className={`${styleTable['selected-row']}`}
                                 key={item._id} onClick={() => onSelected(item)}
                                 data-bs-toggle="modal" data-bs-target="#TicketModal">
