d9b974996bc6b2f5b75e6631779b3750fb804664
Uriel Valderrama <163465994+Uriel767@users.noreply.github.com>
2025-09-25 09:49:08 -0600

    Se aplico el region contextual en los componentes de servicios y tickets, ademas de la creacion de un nuevo endpoint para obtener los paquetes de servicios.

--- OPTICOREAPI/src/middlewares/servicesPackage.routes.js
+++ OPTICOREAPI/src/middlewares/servicesPackage.routes.js
@@ -5,9 +5,7 @@ import { protectRoute, getProfile } from '../controller/auth.controller.js';
 const serviceRoutes = Router();
 
 serviceRoutes.post('/new', protectRoute, getProfile, controller.newService);
-serviceRoutes.get('/all', (req, res) => {
-    res.json([{ test: "ok" }]);
-});
+serviceRoutes.get('/all', controller.viewAllServices);
 serviceRoutes.get('/view/:id', protectRoute, getProfile, controller.viewOneService);
 serviceRoutes.post('/edit/:id', protectRoute, getProfile, controller.editService);
 serviceRoutes.delete('/delete/:id', protectRoute, getProfile, controller.deleteService);
