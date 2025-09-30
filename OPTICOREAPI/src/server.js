276e870960d607279a78b7bc66f5bb37bdb5e3a3
afthon21 <arturotsantos2003@gmail.com>
2025-09-30 10:43:31 -0600

    "modificacion del env"

--- OPTICOREAPI/src/server.js
+++ OPTICOREAPI/src/server.js
@@ -5,7 +5,7 @@ import configureRoutes from './routes/routes.js';
 import cors from 'cors';
 import corsOptions from './libs/cors.js';
 import { iniciarMonitoreoSalud } from './controller/network.controller.js'; // 👈 Agrega esta línea
-import servicesRoutes from "./middlewares/servicesPackage.routes.js";
+import packageRoutes from "./middlewares/packages.routes.js"; // <-- Corrige el nombre aquí
 
 //Config
 dotenv.config();
