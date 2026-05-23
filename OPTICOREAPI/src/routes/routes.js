98a58a7e5985cb30ac525f3f2728938bcb5365db
afthon21 <arturotsantos2003@gmail.com>
2026-05-23 00:45:53 -0600

    Update routes.js

--- OPTICOREAPI/src/routes/routes.js
+++ OPTICOREAPI/src/routes/routes.js
@@ -10,7 +10,6 @@ import packageRoutes from '../middlewares/packages.routes.js';
 import technicianRoutes from '../middlewares/technician.routes.js';
 import logRoutes from '../middlewares/log.routes.js';
 import networkRoutes from '../middlewares/network.routes.js'; //RUTA NETWORK
-import vpnConfigRoutes from '../middlewares/vpnConfig.routes.js';
 
 const configureRoutes = (app) => {
     const principal = '/api'
@@ -27,7 +26,6 @@ const configureRoutes = (app) => {
     app.use(`${principal}/logs`, logRoutes);
     app.use(`${principal}/network`, networkRoutes);
     app.use(`${principal}/tecnicos`, technicianRoutes);
-    app.use(`${principal}/vpn`, vpnConfigRoutes);
 }
 
 export default configureRoutes;
